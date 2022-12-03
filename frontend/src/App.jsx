fdffaef6a5c5883c3551db569248851bef789771
Killian Lanoe <killian.lanoe98@gmail.com>
2022-12-03 01:10:26 +0100

    Initial commit + setup tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+import './styles/tailwind.css';
+
+function App() {
+  return (
+    <div className="flex justify-center h-screen">
+      <div className="w-[90vw]">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
