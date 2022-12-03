d2057f8d80632e6a36e7fa6950bbac07567eac67
Killian Lanoe <killian.lanoe98@gmail.com>
2022-12-03 01:11:09 +0100

    Added Header & Main elements

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../assets/Logo.svg';
+
+function Header() {
+  return (
+    <>
+      <header className="flex justify-between mt-6">
+        <img className="h-12 w-auto" src={logo} alt="Logo Kasa"></img>
+        <nav className="flex">
+          <ul className="flex items-center text-xs text-[#FF6060]">
+            <li className="mr-2 hover:underline">ACCUEIL</li>
+            <li className="hover:underline">À PROPOS</li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default Header;
---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <main>
+      {/* Banner */}
+      <figure>
+        <figcaption
+      </figure>
+      {/* Locations */}
+      <section></section>
+    </main>
+  );
+}
+
+export default Main;
